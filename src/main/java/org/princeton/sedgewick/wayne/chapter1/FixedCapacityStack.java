9fcd028cfcf7b44008d5c36b18267e3a348c5216
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2020-09-28 15:38:29 +0300

    FixedCapacityStack impl

---
+++ src/main/java/org/princeton/sedgewick/wayne/chapter1/FixedCapacityStack.java
@@ -0,0 +1,41 @@
+package org.princeton.sedgewick.wayne.chapter1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class FixedCapacityStack {
+
+    private int N;
+    private String[] arr;
+
+    public FixedCapacityStack(int capacity) {
+        arr = new String[capacity];
+    }
+
+    public void push(String str) {
+        arr[N++] = str;
+    }
+
+    public String pop() {
+        return arr[--N];
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        FixedCapacityStack stack = new FixedCapacityStack(10);
+        stack.push("London");
+        stack.push("is");
+        stack.push("the");
+        stack.push("capital");
+        stack.push("of");
+        stack.push("Great");
+        stack.push("Britain");
+
+        System.out.println(stack.pop()); // Britain
+        System.out.println(stack.pop()); // Great
+        System.out.println(stack.pop()); // of
+        System.out.println(stack.pop()); // capital
+        System.out.println(stack.pop()); // the
+        StdOut.print(stack.pop()); // is
+    }
+}
