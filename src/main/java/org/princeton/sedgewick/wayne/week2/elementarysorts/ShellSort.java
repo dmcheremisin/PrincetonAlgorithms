092412e89b10b2afaf185cae0500951a08995b8e
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-10-28 11:48:10 +0300

    ShellSort update

--- src/main/java/org/princeton/sedgewick/wayne/week2/elementarysorts/ShellSort.java
+++ src/main/java/org/princeton/sedgewick/wayne/week2/elementarysorts/ShellSort.java
@@ -14,15 +14,12 @@ public class ShellSort {
 
     public static void sort(Comparable... arr) {
         int length = arr.length;
-        int h = 1;
-        while (h < length / 3)
-            h = 3 * h + 1;
-        while (h >= 1) {
+
+        for (int h = length / 3; h >= 1; h = h / 3) {
             for (int i = h; i < length; i++) {
                 for (int j = i; j >= h && less(arr[j], arr[j - h]); j -= h)
                     exch(arr, j, j - h);
             }
-            h = h / 3;
         }
     }
 }
