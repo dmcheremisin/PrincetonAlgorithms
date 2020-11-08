3ebf3495093a5304274d96e9bca6090d3a7ff1b0
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-11-08 18:22:22 +0300

    Bottom Up Merge Sort

---
+++ src/main/java/org/princeton/sedgewick/wayne/week3/BottomUpMergeSort.java
@@ -0,0 +1,46 @@
+package org.princeton.sedgewick.wayne.week3;
+
+import static org.princeton.sedgewick.wayne.util.SortUtils.less;
+import static org.princeton.sedgewick.wayne.util.SortUtils.permutation;
+import static org.princeton.sedgewick.wayne.util.SortUtils.printArr;
+
+public class BottomUpMergeSort {
+
+    private static Comparable[] aux;
+
+    public static void main(String[] args) {
+        Integer[] permutation = permutation(20);
+        printArr(permutation);
+        //18 1 8 3 2 7 11 4 10 13 19 15 6 5 12 16 0 14 17 9
+        sort(permutation);
+        printArr(permutation);
+        //0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+    }
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        aux = new Comparable[N];
+        for (int sz = 1; sz < N; sz = sz + sz) // sz: subarray size
+            for (int lo = 0; lo < N - sz; lo += sz + sz) // lo: subarray index
+                merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
+    }
+
+    private static void merge(Comparable[] arr, int lo, int mid, int hi) {
+        for (int k = lo; k <= hi; k++)
+            aux[k] = arr[k];
+
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid)
+                arr[k] = aux[j++];
+            else if (j > hi)
+                arr[k] = aux[i++];
+            else if (less(aux[j], aux[i]))
+                arr[k] = aux[j++];
+            else
+                arr[k] = aux[i++];
+        }
+    }
+
+
+}
