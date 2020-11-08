6ae96ae9353bb4d5a41903e597d56f1c0419a1c0
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-11-08 15:30:06 +0300

    ShellSort step update

--- src/main/java/org/princeton/sedgewick/wayne/week2/elementarysorts/ShellSort.java
+++ src/main/java/org/princeton/sedgewick/wayne/week2/elementarysorts/ShellSort.java
@@ -6,7 +6,7 @@ import static org.princeton.sedgewick.wayne.util.SortUtils.less;
 public class ShellSort {
 
     public static void main(String[] args) {
-        Integer[] arr = {15, 4, 10, 12, 1, 8, 13, 5, 9, 2, 11, 14, 7, 6, 3};
+        Integer[] arr = {15, 4, 10, 12, 1, 8, 13, 5, 9, 2, 11, 14, 7, 6, 3, 17, 16, 18};
         sort(arr);
         for (Integer str : arr)
             System.out.println(str);
@@ -15,7 +15,7 @@ public class ShellSort {
     public static void sort(Comparable... arr) {
         int length = arr.length;
 
-        for (int h = length / 3; h >= 1; h = h / 3) {
+        for (int h = length / 2; h >= 1; h = h / 2) {
             for (int i = h; i < length; i++) {
                 for (int j = i; j >= h && less(arr[j], arr[j - h]); j -= h)
                     exch(arr, j, j - h);
