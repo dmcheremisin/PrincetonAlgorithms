a256c8c8b9e1f9effcc513b38a2293a90f702117
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2020-11-13 22:34:07 +0300

    Collinear points test

--- src/main/java/org/princeton/sedgewick/wayne/week3/challenge/TestBruteCollinearPoints.java
+++ src/main/java/org/princeton/sedgewick/wayne/week3/challenge/TestBruteCollinearPoints.java
@@ -10,14 +10,22 @@ public class TestBruteCollinearPoints {
 
     public static void main(String[] args) {
 
-        int number = 20000;
+        int number = 400;
         int[] pointsCoordinates = SortUtils.randomArr(number, BOUND);
         Point[] points = new Point[number / 2];
         for (int i = 0; i < number; i += 2) {
             int x = pointsCoordinates[i];
             int y = pointsCoordinates[i + 1];
-            points[i/2] = new Point(x, y);
+            points[i / 2] = new Point(x, y);
         }
+        points[10] = new Point(0, 0);
+        points[15] = new Point(1000, 1000);
+        points[24] = new Point(2000, 2000);
+        points[35] = new Point(3000, 3000);
+        points[6] = new Point(5000, 7000);
+        points[18] = new Point(10000, 14000);
+        points[29] = new Point(15000, 21000);
+        points[48] = new Point(20000, 28000);
 
         // draw the points
         StdDraw.enableDoubleBuffering();
