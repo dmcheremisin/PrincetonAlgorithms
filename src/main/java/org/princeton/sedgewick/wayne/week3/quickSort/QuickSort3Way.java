83fc9ea194f2dddd5386fb0a1b1b45923a6b813d
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-11-24 20:20:55 +0300

    QuickSort with 3 way

---
+++ src/main/java/org/princeton/sedgewick/wayne/week3/quickSort/QuickSort3Way.java
@@ -0,0 +1,39 @@
+package org.princeton.sedgewick.wayne.week3.quickSort;
+
+import static org.princeton.sedgewick.wayne.util.SortUtils.exch;
+import static org.princeton.sedgewick.wayne.util.SortUtils.permutation;
+import static org.princeton.sedgewick.wayne.util.SortUtils.printArr;
+
+public class QuickSort3Way {
+
+    public static void main(String[] args) {
+        Integer[] permutation = permutation(20);
+        printArr(permutation);
+        //12 10 8 9 15 5 2 19 6 13 14 7 1 16 4 3 0 18 11 17
+        sort(permutation);
+        printArr(permutation);
+        //0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
+    }
+
+    private static void sort(Comparable[] permutation) {
+        sort(permutation, 0, permutation.length - 1);
+    }
+
+    private static void sort(Comparable[] arr, int lo, int hi) {
+        if (hi <= lo)
+            return;
+
+        int lt = lo, i = lo, gt = hi;
+        Comparable pivot = arr[lo];
+        while (i <= gt) {
+            int cmp = arr[i].compareTo(pivot);
+            if (cmp < 0) exch(arr, lt++, i++);
+            else if (cmp > 0) exch(arr, i, gt--);
+            else i++;
+        }
+
+        sort(arr, lo, lt - 1);
+        sort(arr, gt + 1, hi);
+    }
+
+}
