663b3fefd6ce5bf64d02b999d4ef3d73f4f750a3
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-12-02 19:07:43 +0300

    Empty solver class

---
+++ src/main/java/org/princeton/sedgewick/wayne/week4/challenge/Solver.java
@@ -0,0 +1,30 @@
+package org.princeton.sedgewick.wayne.week4.challenge;
+
+public class Solver {
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial){
+
+    }
+
+    // is the initial board solvable? (see below)
+    public boolean isSolvable(){
+        return false;
+    }
+
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves(){
+        return 0;
+    }
+
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution(){
+        return null;
+    }
+
+    // test client (see below) 
+    public static void main(String[] args){
+
+    }
+
+}
