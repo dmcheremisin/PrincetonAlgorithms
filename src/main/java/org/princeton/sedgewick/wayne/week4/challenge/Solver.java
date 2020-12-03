1f01479b826172e75457339b77718c7b2d1bcd98
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-12-03 19:17:55 +0300

    Solver with test data

--- src/main/java/org/princeton/sedgewick/wayne/week4/challenge/Solver.java
+++ src/main/java/org/princeton/sedgewick/wayne/week4/challenge/Solver.java
@@ -1,30 +1,87 @@
 package org.princeton.sedgewick.wayne.week4.challenge;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Solver {
 
-    // find a solution to the initial board (using the A* algorithm)
-    public Solver(Board initial){
+    private MinPQ<Board> queue;
+    private List<Board> solution;
 
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+        if (initial == null)
+            throw new IllegalArgumentException("Board must not be null");
+
+        queue = new MinPQ<>(Comparator.comparingInt(Board::manhattan));
+        solution = new ArrayList<>();
+
+        queue.insert(initial);
+        while (true) {
+            Board board = queue.delMin();
+            solution.add(board);
+            if (board.isGoal())
+                break;
+
+            Iterable<Board> neighbors = board.neighbors();
+            for (Board neighbor : neighbors) {
+                boolean isDuplicate = false;
+                for (Board solutionBoard : solution) {
+                    if (neighbor.equals(solutionBoard)) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate)
+                    queue.insert(neighbor);
+            }
+        }
     }
 
+
     // is the initial board solvable? (see below)
-    public boolean isSolvable(){
-        return false;
+    public boolean isSolvable() {
+        return true;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
-    public int moves(){
-        return 0;
+    public int moves() {
+        return solution.size();
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
-    public Iterable<Board> solution(){
-        return null;
+    public Iterable<Board> solution() {
+        return solution;
     }
 
     // test client (see below) 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
     }
 
 }
