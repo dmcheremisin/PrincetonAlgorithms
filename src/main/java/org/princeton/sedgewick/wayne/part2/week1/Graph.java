16c6405e64787840097ed17c3901f0e25e7374b3
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2021-01-15 00:09:58 +0300

    p1 w1 Graph

---
+++ src/main/java/org/princeton/sedgewick/wayne/part2/week1/Graph.java
@@ -0,0 +1,54 @@
+package org.princeton.sedgewick.wayne.part2.week1;
+
+import edu.princeton.cs.algs4.In;
+import org.princeton.sedgewick.wayne.part1.week2.containers.bag.Bag;
+
+public class Graph {
+
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    private Graph(int V) {
+        this.V = V;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int i = 0; i < V; i++)
+            adj[i] = new Bag<>();
+    }
+
+    public Graph(In in) {
+        this(in.readInt()); // read vertices
+        int E = in.readInt(); // read edges
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            addEdge(v, w);
+        }
+    }
+
+    public int getV() {
+        return V;
+    }
+
+    public int getE() {
+        return E;
+    }
+
+    private void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Graph graph = new Graph(in);
+        graph.adj(0).forEach(v -> System.out.printf("%1$2s", v)); //  6 2 1 5
+        System.out.println();
+        graph.adj(5).forEach(v -> System.out.printf("%1$2s", v)); //  3 4 0
+    }
+}
