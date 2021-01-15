ab99528cc80bdbe9e5cf5ff0c45b9895ab9fa1a1
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2021-01-16 00:33:50 +0300

    p1 w1 directed dfs

---
+++ src/main/java/org/princeton/sedgewick/wayne/part2/week1/DirectedDepthFirstSearch.java
@@ -0,0 +1,56 @@
+package org.princeton.sedgewick.wayne.part2.week1;
+
+import edu.princeton.cs.algs4.In;
+
+public class DirectedDepthFirstSearch {
+
+    private final Digraph digraph;
+    private final boolean[] marked;
+
+    public DirectedDepthFirstSearch(Digraph digraph, int s) {
+        this.digraph = digraph;
+        marked = new boolean[digraph.getV()];
+        dfs(s);
+    }
+
+    public DirectedDepthFirstSearch(Digraph digraph, Iterable<Integer> sources) {
+        this.digraph = digraph;
+        marked = new boolean[digraph.getV()];
+        for (Integer s : sources)
+            if (!marked[s])
+                dfs(s);
+    }
+
+    private void dfs(int s) {
+        System.out.printf("Visiting vertex: %s \n", s);
+        marked[s] = true;
+        for (Integer w : digraph.adj(s))
+            if (!marked[w])
+                dfs(w);
+    }
+
+    public boolean marked(int s) {
+        return marked[s];
+    }
+
+    public static void main(String[] args) {
+        Digraph digraph = new Digraph(new In(args[0]));
+        DirectedDepthFirstSearch dfs1 = new DirectedDepthFirstSearch(digraph, 0);
+        //Visiting vertex: 0
+        //Visiting vertex: 6
+        //Visiting vertex: 4
+        //Visiting vertex: 3
+        //Visiting vertex: 2
+        //Visiting vertex: 1
+        //Visiting vertex: 5
+        System.out.println(dfs1.marked(3)); // true
+        System.out.println(dfs1.marked(11)); // false
+
+        DirectedDepthFirstSearch dfs2 = new DirectedDepthFirstSearch(digraph, 9);
+        //Visiting vertex: 9
+        //Visiting vertex: 11
+        //Visiting vertex: 12
+        //Visiting vertex: 10
+    }
+
+}
