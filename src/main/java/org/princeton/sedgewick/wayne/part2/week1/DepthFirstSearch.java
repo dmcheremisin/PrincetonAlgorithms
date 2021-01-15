200363e4ce6b581060291a68e856cf41a815a99b
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2021-01-15 13:01:02 +0300

    p2 week1 depth first search

---
+++ src/main/java/org/princeton/sedgewick/wayne/part2/week1/DepthFirstSearch.java
@@ -0,0 +1,50 @@
+package org.princeton.sedgewick.wayne.part2.week1;
+
+import edu.princeton.cs.algs4.In;
+
+public class DepthFirstSearch {
+
+    private boolean marked[];
+    private int count;
+
+    public DepthFirstSearch(Graph g, int s) {
+        marked = new boolean[g.getV()];
+        dfs(g, s);
+    }
+
+    private void dfs(Graph g, int s) {
+        marked[s] = true;
+        System.out.printf("Visiting vertex: %s \n", s);
+        count++;
+        for (Integer adj : g.adj(s))
+            if (!marked[adj])
+                dfs(g, adj);
+    }
+
+    public boolean marked(int w) {
+        return marked[w];
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(new In(args[0]));
+        DepthFirstSearch dfs = new DepthFirstSearch(graph, 0);
+        //Visiting vertex: 0
+        //Visiting vertex: 6
+        //Visiting vertex: 4
+        //Visiting vertex: 5
+        //Visiting vertex: 3
+        //Visiting vertex: 2
+        //Visiting vertex: 1
+        System.out.println(dfs.count()); // 7
+        System.out.println(dfs.marked(3)); // true
+        System.out.println(dfs.marked(11)); // false
+        if (dfs.count() != graph.getV())
+            System.out.print("NOT ");
+        System.out.println("connected");
+        // NOT connected
+    }
+}
