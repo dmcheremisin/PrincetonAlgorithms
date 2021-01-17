003a294c1e4082662330527c9d865f57d124fb49
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2021-01-17 13:44:00 +0300

    p2 week1 digraph depth first order

---
+++ src/main/java/org/princeton/sedgewick/wayne/part2/week1/DigraphDepthFirstOrder.java
@@ -0,0 +1,66 @@
+package org.princeton.sedgewick.wayne.part2.week1;
+
+import edu.princeton.cs.algs4.In;
+import org.princeton.sedgewick.wayne.part1.week2.containers.bag.Bag;
+import org.princeton.sedgewick.wayne.part1.week2.containers.queue.ListQueue;
+
+public class DigraphDepthFirstOrder {
+
+    private final Digraph digraph;
+    private final boolean marked[];
+
+    private final ListQueue<Integer> pre;   // queue
+    private final ListQueue<Integer> post;  // queue
+    private final Bag<Integer> reversePost; // stack
+
+    public DigraphDepthFirstOrder(Digraph digraph) {
+        this.digraph = digraph;
+        this.marked = new boolean[digraph.getV()];
+
+        pre = new ListQueue<>();
+        post = new ListQueue<>();
+        reversePost = new Bag<>();
+
+        for (int v = 0; v < digraph.getV(); v++)
+            if (!marked[v])
+                dfs(v);
+    }
+
+    private void dfs(int v) {
+        pre.enqueue(v);
+
+        marked[v] = true;
+        for (Integer w : digraph.adj(v))
+            if (!marked[w])
+                dfs(w);
+
+        post.enqueue(v);
+        reversePost.add(v);
+    }
+
+    public Iterable<Integer> pre() {
+        return pre;
+    }
+
+    public Iterable<Integer> post() {
+        return post;
+    }
+
+    public Iterable<Integer> reversePost() {
+        return reversePost;
+    }
+
+    public static void main(String[] args) {
+        Digraph graph = new Digraph(new In(args[0]));
+        DigraphDepthFirstOrder order = new DigraphDepthFirstOrder(graph);
+
+        System.out.println(order.pre);
+        //[0, 5, 4, 3, 2, 1, 6, 9, 11, 12, 10, 8, 7]
+
+        System.out.println(order.post);
+        //[2, 3, 4, 5, 1, 0, 12, 11, 10, 9, 8, 6, 7]
+
+        System.out.println(order.reversePost);
+        //[7, 6, 8, 9, 10, 11, 12, 0, 1, 5, 4, 3, 2]
+    }
+}
