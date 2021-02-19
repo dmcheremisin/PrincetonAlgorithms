86e93ea3936536a94bdb02f1fb2f4839c4a4c051
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2021-02-19 19:41:12 +0300

    p2 week3 BaseballElimination non trivial elimination

--- src/main/java/org/princeton/sedgewick/wayne/part2/week3/minCutMaxFlow/challenge/BaseballElimination.java
+++ src/main/java/org/princeton/sedgewick/wayne/part2/week3/minCutMaxFlow/challenge/BaseballElimination.java
@@ -1,5 +1,8 @@
 package org.princeton.sedgewick.wayne.part2.week3.minCutMaxFlow.challenge;
 
+import edu.princeton.cs.algs4.FlowEdge;
+import edu.princeton.cs.algs4.FlowNetwork;
+import edu.princeton.cs.algs4.FordFulkerson;
 import edu.princeton.cs.algs4.In;
 
 import java.util.ArrayList;
@@ -100,10 +103,7 @@ public class BaseballElimination {
 
     private List<String> getTrivialElimination(String team) {
         int teamIndex = getTeamIndex(team);
-        int[] teamScore = score[teamIndex];
-        int wins = teamScore[0];
-        int remaining = teamScore[2];
-        int possibleWins = wins + remaining;
+        int possibleWins = getPossibleWins(teamIndex);
 
         List<String> eliminatingTeams = new ArrayList<>();
         for (int i = 0; i < v && i != teamIndex; i++) {
@@ -113,6 +113,66 @@ public class BaseballElimination {
         }
 
         return eliminatingTeams;
+    }
+
+    private int getPossibleWins(int teamIndex) {
+        int[] teamScore = score[teamIndex];
+        int wins = teamScore[0];
+        int remaining = teamScore[2];
+        return wins + remaining;
+    }
+
+    public List<FlowEdge> getNonTrivialElimination(String team) {
+        int teamIndex = getTeamIndex(team);
+        int possibleWins = getPossibleWins(teamIndex);
+
+        int gameVertexes = getGameVertexes();
+        int totalVertexes = 1 + gameVertexes + v + 1;
+        FlowNetwork flowNetwork = new FlowNetwork(totalVertexes);
+        List<FlowEdge> gameEdges = new ArrayList<>();
+
+        int s = totalVertexes - 2;
+        int t = totalVertexes - 1;
+
+        int gameVertex = 0;
+        for (int i = 0; i < v; i++) {
+            if (i == teamIndex)
+                continue;
+
+            int[] teamSchedule = schedule[i];
+            int firstTeamVertex = gameVertexes + i - 1;
+
+            for (int j = i + 1; j < v; j++) {
+                if (j == teamIndex)
+                    continue;
+
+                FlowEdge sourceGameEdge = new FlowEdge(s, gameVertex, teamSchedule[j]);
+                flowNetwork.addEdge(sourceGameEdge);
+                gameEdges.add(sourceGameEdge);
+
+                FlowEdge gameFirstTeamEdge = new FlowEdge(gameVertex, firstTeamVertex, Double.POSITIVE_INFINITY);
+                flowNetwork.addEdge(gameFirstTeamEdge);
+
+                FlowEdge gameSecondTeamEdge = new FlowEdge(gameVertex, gameVertexes + j - 1, Double.POSITIVE_INFINITY);
+                flowNetwork.addEdge(gameSecondTeamEdge);
+            }
+
+            int firstTeamWins = score[i][0];
+            FlowEdge firstTeamToTargetEdge = new FlowEdge(firstTeamVertex, t, possibleWins - firstTeamWins);
+            flowNetwork.addEdge(firstTeamToTargetEdge);
+
+            gameVertex++;
+        }
+        FordFulkerson fordFulkerson = new FordFulkerson(flowNetwork, s, t);
+
+        return gameEdges;
+    }
+
+    private int getGameVertexes() {
+        int gameVertexes = 0;
+        for (int i = v; i - 2 > 0; i--)
+            gameVertexes += (i - 2);
+        return gameVertexes;
     }
 
     public static void main(String[] args) {
@@ -126,17 +186,22 @@ public class BaseballElimination {
         //Montreal
         //Philadelphia
 
-        System.out.println(elimination.numberOfTeams()); // 4
-        System.out.println(elimination.wins("Philadelphia")); // 80
-        System.out.println(elimination.losses("Philadelphia")); // 79
-        System.out.println(elimination.remaining("Philadelphia")); // 3
-        System.out.println(elimination.against("Atlanta", "Philadelphia")); // 1
+//        System.out.println(elimination.numberOfTeams()); // 4
+//        System.out.println(elimination.wins("Philadelphia")); // 80
+//        System.out.println(elimination.losses("Philadelphia")); // 79
+//        System.out.println(elimination.remaining("Philadelphia")); // 3
+//        System.out.println(elimination.against("Atlanta", "Philadelphia")); // 1
+//
+//        System.out.println(elimination.isEliminated("Montreal")); // true
+//        System.out.println(elimination.certificateOfElimination("Montreal")); // [Atlanta]
+//
+//        System.out.println(elimination.isEliminated("New_York")); // false
+//        System.out.println(elimination.certificateOfElimination("New_York")); // null
 
-        System.out.println(elimination.isEliminated("Montreal")); // true
-        System.out.println(elimination.certificateOfElimination("Montreal")); // [Atlanta]
+        for (FlowEdge edge : elimination.getNonTrivialElimination("Detroit")) {
+            System.out.println(edge);
+        }
 
-        System.out.println(elimination.isEliminated("New_York")); // false
-        System.out.println(elimination.certificateOfElimination("New_York")); // null
     }
 
 }
