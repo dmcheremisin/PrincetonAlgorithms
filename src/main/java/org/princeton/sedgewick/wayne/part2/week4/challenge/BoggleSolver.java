c284e4dd936f5c9c8d69c0723e6e7930187600b0
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2021-02-28 15:36:34 +0300

    p2 w4 boggle challenge

--- src/main/java/org/princeton/sedgewick/wayne/part2/week4/challenge/BoggleSolver.java
+++ src/main/java/org/princeton/sedgewick/wayne/part2/week4/challenge/BoggleSolver.java
@@ -1,7 +1,6 @@
 package org.princeton.sedgewick.wayne.part2.week4.challenge;
 
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.TST;
 
 import java.util.HashSet;
@@ -24,25 +23,93 @@ public class BoggleSolver {
         int cols = board.cols();
         Set<String> validWords = new HashSet<>();
 
-        Queue<String> prefixes = new Queue<>();
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                char letter = board.getLetter(i, j);
+        boolean[][] visited = new boolean[rows][];
+        for (int i = 0; i < rows; i++)
+            visited[i] = new boolean[cols];
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                String prefix = board.getLetter(row, col) + "";
+                getValidNeighbors(row, col, copy2dArray(visited), prefix, board, validWords);
             }
         }
-        return null;
+
+        return validWords;
+    }
+
+    private void getValidNeighbors(int row, int col, boolean[][] visited, String prefix,
+                                   BoggleBoard board, Set<String> validWords) {
+        if (visited[row][col])
+            return;
+
+        visited[row][col] = true;
+
+        if (words.contains(prefix))
+            validWords.add(prefix);
+
+        for (int i = -1; i < 2; i++) {
+            int iRow = row + i;
+            if (iRow < 0 || iRow >= board.rows())
+                continue;
+
+            for (int j = -1; j < 2; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+
+                int jCol = col + j;
+                if (jCol < 0 || jCol >= board.cols())
+                    continue;
+
+                char letter = board.getLetter(iRow, jCol);
+
+                String newPrefix = letter == 'Q' ? prefix + letter + 'U' : prefix + letter;
+
+                if (words.keysWithPrefix(newPrefix).iterator().hasNext())
+                    getValidNeighbors(iRow, jCol, copy2dArray(visited), newPrefix, board, validWords);
+            }
+        }
+    }
+
+    private boolean[][] copy2dArray(boolean[][] arr) {
+        boolean[][] newArr = new boolean[arr.length][];
+        for (int i = 0; i < arr.length; i++)
+            newArr[i] = arr[i].clone();
+        return newArr;
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
-        return 0;
+        if (!words.contains(word))
+            return 0;
+
+        int length = word.length();
+        if (length < 3)
+            return 0;
+        else if (length <= 4)
+            return 1;
+        else if (length == 5)
+            return 2;
+        else if (length == 6)
+            return 3;
+        else if (length == 7)
+            return 5;
+
+        return 11;
     }
 
     public static void main(String[] args) {
-        In in = new In(args[0]);
-        String[] strings = in.readAllStrings();
+        String basePath = "C:\\Workspace\\PrincetonAlgorithms\\src\\main\\resources\\part2\\week4\\challenge\\";
+        String[] strings = new In(basePath + args[0]).readAllStrings();
         BoggleSolver boggleSolver = new BoggleSolver(strings);
+        BoggleBoard boggleBoard = new BoggleBoard(basePath + args[1]);
+        Set<String> allValidWords = (Set<String>) boggleSolver.getAllValidWords(boggleBoard);
+        allValidWords.forEach(System.out::println);
+        System.out.println("Size: " + allValidWords.size());
 
+        int score = allValidWords.stream()
+                .mapToInt(boggleSolver::scoreOf)
+                .sum();
+        System.out.println("Score: " + score);
     }
 }
