7274cab01eff57a8c4f69c45dd6eb33f121abf6f
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2021-03-05 16:57:29 +0300

    p2 w5 data compression challenge

---
+++ src/main/java/org/princeton/sedgewick/wayne/part2/week5/challenge/MoveToFront.java
@@ -0,0 +1,21 @@
+package org.princeton.sedgewick.wayne.part2.week5.challenge;
+
+public class MoveToFront {
+
+    // apply move-to-front encoding, reading from standard input and writing to standard output
+    public static void encode() {
+    }
+
+    // apply move-to-front decoding, reading from standard input and writing to standard output
+    public static void decode() {
+    }
+
+    // if args[0] is "-", apply move-to-front encoding
+    // if args[0] is "+", apply move-to-front decoding
+    public static void main(String[] args) {
+        for (int i = 0; i < 256; i++) {
+            System.out.println((char) i);
+        }
+    }
+
+}
