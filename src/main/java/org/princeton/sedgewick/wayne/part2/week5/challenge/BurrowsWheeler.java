e64cea9106c3dabffce0d47889db4ad4d1d05ccb
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2021-03-05 19:41:41 +0300

    p2 w5 BurrowsWheeler

--- src/main/java/org/princeton/sedgewick/wayne/part2/week5/challenge/BurrowsWheeler.java
+++ src/main/java/org/princeton/sedgewick/wayne/part2/week5/challenge/BurrowsWheeler.java
@@ -1,22 +1,31 @@
 package org.princeton.sedgewick.wayne.part2.week5.challenge;
 
+import edu.princeton.cs.algs4.BinaryStdIn;
+
 public class BurrowsWheeler {
 
     // apply Burrows-Wheeler transform,
     // reading from standard input and writing to standard output 
-    public static void transform(){
-
+    public static void transform() {
+        String str = BinaryStdIn.readString();
+        System.out.println(str);
     }
 
     // apply Burrows-Wheeler inverse transform,
     // reading from standard input and writing to standard output
-    public static void inverseTransform(){
+    public static void inverseTransform() {
 
     }
 
     // if args[0] is "-", apply Burrows-Wheeler transform
     // if args[0] is "+", apply Burrows-Wheeler inverse transform
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        if (args[0].equals("-"))
+            transform();
+        else if (args[0].equals("+"))
+            inverseTransform();
+        else
+            throw new IllegalArgumentException("Illegal command line argument");
 
     }
 
