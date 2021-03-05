8fd27970fa1425e3797e3744a7e82fc9b5bb9325
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2021-03-05 22:45:49 +0300

    p2 w5 BurrowsWheeler transform

--- src/main/java/org/princeton/sedgewick/wayne/part2/week5/challenge/BurrowsWheeler.java
+++ src/main/java/org/princeton/sedgewick/wayne/part2/week5/challenge/BurrowsWheeler.java
@@ -1,6 +1,7 @@
 package org.princeton.sedgewick.wayne.part2.week5.challenge;
 
 import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
 
 public class BurrowsWheeler {
 
@@ -8,13 +9,30 @@ public class BurrowsWheeler {
     // reading from standard input and writing to standard output 
     public static void transform() {
         String str = BinaryStdIn.readString();
-        System.out.println(str);
+
+        CircularSuffixArray circularSuffixArray = new CircularSuffixArray(str);
+
+        StringBuilder sb = new StringBuilder();
+        int position = 0;
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            int index = circularSuffixArray.index(i);
+            if (index == 0)
+                position = i;
+            index = index > 0 ? index - 1 : length - 1;
+            sb.append(str.charAt(index));
+        }
+
+        BinaryStdOut.write(position);
+        BinaryStdOut.write(sb.toString());
+        BinaryStdOut.close();
     }
 
     // apply Burrows-Wheeler inverse transform,
     // reading from standard input and writing to standard output
     public static void inverseTransform() {
 
+        BinaryStdOut.close();
     }
 
     // if args[0] is "-", apply Burrows-Wheeler transform
