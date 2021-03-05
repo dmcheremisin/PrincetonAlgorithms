9fd3cffff0cfcfbd177295e5d4af3c0bcd85dd99
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2021-03-06 00:05:11 +0300

    p2 w5 fixed star import

--- src/main/java/org/princeton/sedgewick/wayne/part2/week5/challenge/CircularSuffixArray.java
+++ src/main/java/org/princeton/sedgewick/wayne/part2/week5/challenge/CircularSuffixArray.java
@@ -1,6 +1,10 @@
 package org.princeton.sedgewick.wayne.part2.week5.challenge;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CircularSuffixArray {
 
