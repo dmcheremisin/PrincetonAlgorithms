a74c94632c8f84212c5acb1959600e37338b8db1
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2021-03-05 18:20:44 +0300

    p2 w5 data MoveToFront

--- src/main/java/org/princeton/sedgewick/wayne/part2/week5/challenge/HexDump.java
+++ src/main/java/org/princeton/sedgewick/wayne/part2/week5/challenge/HexDump.java
@@ -71,9 +71,13 @@ public class HexDump {
                 BinaryStdIn.readChar();
                 continue;
             }
-            if (i == 0) StdOut.printf("");
-            else if (i % bytesPerLine == 0) StdOut.printf("\n", i);
-            else StdOut.print(" ");
+            if (i == 0)
+                StdOut.printf("");
+            else if (i % bytesPerLine == 0)
+                StdOut.printf("\n", i);
+            else
+                StdOut.print(" ");
+
             char c = BinaryStdIn.readChar();
             StdOut.printf("%02x", c & 0xff);
         }
--- src/main/java/org/princeton/sedgewick/wayne/part2/week5/challenge/MoveToFront.java
+++ src/main/java/org/princeton/sedgewick/wayne/part2/week5/challenge/MoveToFront.java
@@ -1,21 +1,57 @@
 package org.princeton.sedgewick.wayne.part2.week5.challenge;
 
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
+import java.util.LinkedList;
+
 public class MoveToFront {
 
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
+        LinkedList<Character> charStack = getCharStack();
+        while (!BinaryStdIn.isEmpty()) {
+            char c = BinaryStdIn.readChar();
+            int index = charStack.indexOf(c);
+            BinaryStdOut.write(index, 8);
+
+            charStack.remove(index);
+            charStack.push(c);
+        }
+        BinaryStdOut.close();
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
     public static void decode() {
+        LinkedList<Character> charStack = getCharStack();
+        while (!BinaryStdIn.isEmpty()) {
+            int charIndex = BinaryStdIn.readInt(8);
+            Character aChar = charStack.remove(charIndex);
+
+            BinaryStdOut.write(aChar, 8);
+            charStack.push(aChar);
+        }
+        BinaryStdOut.close();
     }
 
     // if args[0] is "-", apply move-to-front encoding
     // if args[0] is "+", apply move-to-front decoding
     public static void main(String[] args) {
+        if (args[0].equals("-"))
+            encode();
+        else if (args[0].equals("+"))
+            decode();
+        else
+            throw new IllegalArgumentException("Illegal command line argument");
+    }
+
+    private static LinkedList<Character> getCharStack() {
+        LinkedList<Character> stack = new LinkedList<>();
         for (int i = 0; i < 256; i++) {
-            System.out.println((char) i);
+            char aChar = (char) i;
+            stack.add(aChar);
         }
+        return stack;
     }
 
 }
