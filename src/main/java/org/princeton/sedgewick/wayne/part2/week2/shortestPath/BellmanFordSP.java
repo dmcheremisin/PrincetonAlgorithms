ccd14764a99381c0480afbcfbef1991ce128b58b
Dmitry Cheremisin <Dmitry.Cheremisin@infobip.com>
2022-02-08 17:57:41 +0300

    BellmanFord reviewed

--- src/main/java/org/princeton/sedgewick/wayne/part2/week2/shortestPath/BellmanFordSP.java
+++ src/main/java/org/princeton/sedgewick/wayne/part2/week2/shortestPath/BellmanFordSP.java
@@ -1,99 +1,49 @@
 package org.princeton.sedgewick.wayne.part2.week2.shortestPath;
 
+import edu.princeton.cs.algs4.*;
 import edu.princeton.cs.algs4.DirectedEdge;
 import edu.princeton.cs.algs4.EdgeWeightedDigraph;
-import edu.princeton.cs.algs4.EdgeWeightedDirectedCycle;
-import edu.princeton.cs.algs4.Queue;
-
 
 public class BellmanFordSP {
+    private final DirectedEdge[] edgeTo;
+    private final double[] distTo;
 
-    // for floating-point precision issues
-    private static final double EPSILON = 1E-14;
-
-    private double[] distTo;               // distTo[v] = distance  of shortest s->v path
-    private DirectedEdge[] edgeTo;         // edgeTo[v] = last edge on shortest s->v path
-    private boolean[] onQueue;             // onQueue[v] = is v currently on the queue?
-    private Queue<Integer> queue;          // queue of vertices to relax
-    private int cost;                      // number of calls to relax()
-    private Iterable<DirectedEdge> cycle;  // negative cycle (or null if no such cycle)
-
-    /**
-     * Computes a shortest paths tree from {@code s} to every other vertex in
-     * the edge-weighted digraph {@code G}.
-     *
-     * @param G the acyclic digraph
-     * @param s the source vertex
-     * @throws IllegalArgumentException unless {@code 0 <= s < V}
-     */
     public BellmanFordSP(EdgeWeightedDigraph G, int s) {
-        distTo = new double[G.V()];
         edgeTo = new DirectedEdge[G.V()];
-        onQueue = new boolean[G.V()];
+        distTo = new double[G.V()];
+
         for (int v = 0; v < G.V(); v++)
             distTo[v] = Double.POSITIVE_INFINITY;
-        distTo[s] = 0.0;
+        distTo[s] = 0;
 
-        // Bellman-Ford algorithm
-        queue = new Queue<>();
-        queue.enqueue(s);
-        onQueue[s] = true;
-        while (!queue.isEmpty() && !hasNegativeCycle()) {
-            int v = queue.dequeue();
-            onQueue[v] = false;
-            relax(G, v);
+        // Complexity: worst case == average case == E * V
+        for (int i = 0; i < G.V(); i++) // cycle that iterates over all vertexes V
+            for (int v = 0; v < G.V(); v++) // cycle that iterates over all edges E
+                for (DirectedEdge e : G.adj(v))
+                    relax(e);
+    }
+
+    private void relax(DirectedEdge e) {
+        int v = e.from(), w = e.to();
+        if (distTo[w] > distTo[v] + e.weight()) {
+            distTo[w] = distTo[v] + e.weight();
+            edgeTo[w] = e;
         }
     }
 
-    // relax vertex v and put other endpoints on queue if changed
-    private void relax(EdgeWeightedDigraph G, int v) {
-        for (DirectedEdge e : G.adj(v)) {
-            int w = e.to();
-            if (distTo[w] > distTo[v] + e.weight() + EPSILON) {
-                distTo[w] = distTo[v] + e.weight();
-                edgeTo[w] = e;
-                if (!onQueue[w]) {
-                    queue.enqueue(w);
-                    onQueue[w] = true;
-                }
-            }
-            if (++cost % G.V() == 0) {
-                findNegativeCycle();
-                if (hasNegativeCycle()) return;  // found a negative cycle
-            }
-        }
+    public double distTo(int v) {
+        return distTo[v];
     }
 
-    /**
-     * Is there a negative cycle reachable from the source vertex {@code s}?
-     *
-     * @return {@code true} if there is a negative cycle reachable from the
-     * source vertex {@code s}, and {@code false} otherwise
-     */
-    public boolean hasNegativeCycle() {
-        return cycle != null;
+    public boolean hasPathTo(int v) {
+        return distTo[v] < Double.POSITIVE_INFINITY;
     }
 
-    /**
-     * Returns a negative cycle reachable from the source vertex {@code s}, or {@code null}
-     * if there is no such cycle.
-     *
-     * @return a negative cycle reachable from the soruce vertex {@code s}
-     * as an iterable of edges, and {@code null} if there is no such cycle
-     */
-    public Iterable<DirectedEdge> negativeCycle() {
-        return cycle;
-    }
-
-    // by finding a cycle in predecessor graph
-    private void findNegativeCycle() {
-        int V = edgeTo.length;
-        EdgeWeightedDigraph spt = new EdgeWeightedDigraph(V);
-        for (int v = 0; v < V; v++)
-            if (edgeTo[v] != null)
-                spt.addEdge(edgeTo[v]);
-
-        EdgeWeightedDirectedCycle finder = new EdgeWeightedDirectedCycle(spt);
-        cycle = finder.cycle();
+    public Iterable<DirectedEdge> getPathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        Stack<DirectedEdge> path = new Stack<>();
+        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()])
+            path.push(e);
+        return path;
     }
 }
